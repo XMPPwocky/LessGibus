246373022a468a9c1ef406a9a707ed99956ecfb7
XMPPwocky <nttheis@gmail.com>
2013-07-24 00:11:05 -0700

    Added trivial test framework

---
+++ LessGibus/core_tests/core_tests.cpp
@@ -0,0 +1,12 @@
+// core_tests.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::cout << "All tests passed!";
+	getc(stdin);
+}
+
