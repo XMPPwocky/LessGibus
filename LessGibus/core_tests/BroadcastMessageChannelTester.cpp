3f623772bf4588839720e3d64f47c34a8489ebd9
XMPPwocky <nttheis@gmail.com>
2013-07-25 10:15:40 -0700

    rename a dummy variable

--- LessGibus/core_tests/BroadcastMessageChannelTester.cpp
+++ LessGibus/core_tests/BroadcastMessageChannelTester.cpp
@@ -13,5 +13,8 @@ BroadcastMessageChannelTester::~BroadcastMessageChannelTester(void)
 
 void BroadcastMessageChannelTester::test(void)
 {
-	shared_ptr<BroadcastMessageChannel> msg_ptr(new BroadcastMessageChannel);
+	shared_ptr<BroadcastMessageChannel> channel_ptr(new BroadcastMessageChannel);
+	BroadcastMessageChannel channel = *channel_ptr;
+
+
 }
